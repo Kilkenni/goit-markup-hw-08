ffb2a850a43cec358f89bd1da976b127b1541347
Kilkenni <hpittman89@outlook.com>
2021-09-02 23:31:01 +0300

    adaptive - modal window

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    // body: document.querySelector('[data-body]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,8 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    // refs.body.classList.toggle('is-overflow-hidden');
+    document.body.classList.toggle("is-overflow-hidden");
+
   }
 })();
