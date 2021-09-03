767e0f94b8006d602d8f092048375e4805f2c25b
Kilkenni <hpittman89@outlook.com>
2021-09-03 08:19:34 +0300

    reworked to responsive, nav placeholder

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,28 @@
     // body: document.querySelector('[data-body]'),
   };
 
+  
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
     // refs.body.classList.toggle('is-overflow-hidden');
     document.body.classList.toggle("is-overflow-hidden");
+  }
 
+  const menuBtn = document.querySelector('[data-menu-button]');
+  const menuRef = document.querySelector('[data-menu]');
+  const contactsRef = document.querySelector('[data-contacts]');
+  menuBtn.addEventListener("click", toggleNavMenu);
+
+  function toggleNavMenu() {
+    menuBtn.classList.toggle("is-open");
+    menuRef.classList.toggle("is-open");
+    contactsRef.classList.toggle("is-open");
+    document.body.classList.toggle("is-overflow-hidden");
+    const expanded = menuBtn.getAttribute("aria-expanded") === "true" || false;
+    menuBtn.setAttribute("aria-expanded", !expanded)
   }
 })();
